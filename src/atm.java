86be3fe27148bde72a5a82f21d5a76c941373ca2
Alex-Shane <ashane1230@gmail.com>
2021-08-29 12:04:12 -0700

    Fixed all errors and warnings. Tested multiple cases and was successful with deposit method.

--- src/atm.java
+++ src/atm.java
@@ -2,24 +2,28 @@ import java.util.HashMap;
 
 public class atm {
 	private String iD;
-	private static HashMap bankAccounts; 
-	bankAccounts = new HashMap();
-	
-	public atm (String iD)
-	{
-		this.iD = iD;
-	}
+	private HashMap <String,Double> bankAccounts = new HashMap <String,Double>(); 
 	public void deposit(long iD, double amount)
 	{
 		this.iD = "" + iD;
-		double oldMoney = (double) bankAccounts.get(this.iD);
-		if (bankAccounts.containsKey((this.iD)))
+		if (bankAccounts.isEmpty() || !bankAccounts.containsKey((this.iD)))
 		{
-			bankAccounts.put(this.iD, oldMoney + amount);
+			bankAccounts.put(this.iD, amount);
+			//System.out.println ("ID didn't exist. Created and Deposited " + amount);
 		}
 		else
 		{
-			bankAccounts.put(this.iD, amount);
+			double oldMoney = (double) bankAccounts.get(this.iD);
+			bankAccounts.put(this.iD, oldMoney + amount);
+			//System.out.println ("ID existed. Deposited " + amount + ". There is now " + bankAccounts.get(this.iD) + " in this account.");
 		}
 	}
+	/*public static void main (String [] args)
+	{
+		atm a = new atm ();
+		a.deposit(505, 100);
+		a.deposit(202, 50);
+		a.deposit(1234567812, 1);
+		a.deposit(505, 10);
+	}*/
 }
